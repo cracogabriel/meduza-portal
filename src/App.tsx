726602133a24fcd4c2d5e9455206ca975b471b1e
craco <gabriel_craco@hotmail.com>
2022-09-03 20:22:40 -0300

    chore: add react-router and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from './views/home'
+import Login from './views/login'
+
 function App() {
-  return <div></div>
+  return (
+    <Routes>
+      <Route path='login' element={<Login />} />
+      <Route path='home' element={<Home />} />
+      <Route path='*' element={<Navigate to='/login' replace />} />
+    </Routes>
+  )
 }
 
 export default App
