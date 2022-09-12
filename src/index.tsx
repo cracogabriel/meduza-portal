ef99627365a35095f6fe08e2d77557efd3bd78f1
craco <gabriel_craco@hotmail.com>
2022-09-12 20:15:18 -0300

    feat: reset css style

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './style.css'
+import './reset.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <BrowserRouter>
-    <div className={'style'}>
+    <div className={'reset'}>
       <App />
     </div>
   </BrowserRouter>,
