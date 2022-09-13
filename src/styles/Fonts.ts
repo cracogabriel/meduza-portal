f7d81c930250860997de4b573ad5b7a9dc21c7cf
craco <gabriel_craco@hotmail.com>
2022-09-12 21:00:47 -0300

    chore: add global fonts

---
+++ src/styles/Fonts.ts
@@ -0,0 +1,13 @@
+import { css } from 'styled-components'
+
+export const YuseiMagic400 = css`
+  @import url('https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap');
+`
+
+export const Ubuntu400 = css`
+  @import url('https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap');
+`
+
+export const Ubuntu700 = css`
+  @import url('https://fonts.googleapis.com/css2?family=Ubuntu+Mono:wght@700&display=swap');
+`
