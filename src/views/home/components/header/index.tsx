dae98cd6fc15baa1d6bf520e17cde5b67e3df419
craco <gabriel_craco@hotmail.com>
2022-09-12 21:00:11 -0300

    feat: create header

---
+++ src/views/home/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { HeaderContainer } from './style'
+
+function Header() {
+  return (
+    <HeaderContainer>
+      <p>teste</p>
+      <button>teste</button>
+    </HeaderContainer>
+  )
+}
+
+export default Header
---
+++ src/views/home/components/header/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  height: 36px;
+  width: 100%;
+  padding: 0 52px;
+  margin-top: 35px;
+  justify-content: space-between;
+`
