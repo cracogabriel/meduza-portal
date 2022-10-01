0f3423106cb7158e5c7b20c31e488450fafc0e3b
craco <gabriel_craco@hotmail.com>
2022-10-01 17:03:15 -0300

    refactor: remove login functions from App.tsx

--- src/views/register/index.tsx
+++ src/views/register/index.tsx
@@ -1,33 +1,19 @@
-import axios from 'axios'
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import Button from '../../components/Button'
 import LoginInput from '../../components/LoginInput'
 import { Background, FormContainer, ImageLogin, LoginLogo } from './styles'
 
-function Login() {
+type Props = {
+  handleLogin: (email: string, password: string) => Promise<void>
+}
+
+function Login({ handleLogin }: Props) {
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   const emailRegex: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
 
   const loginError = emailRegex.test(email) || email.length === 0 ? '' : 'email inválido'
   const passwordError = ''
-
-  const navigate = useNavigate()
-
-  const handleLogin = async (email: string, password: string) => {
-    try {
-      const response = await axios.post('http://26.181.166.34:8080/api/gym/login', {
-        gym_email: email,
-        gym_password: password,
-      })
-      localStorage.setItem('token', response.data.token)
-      localStorage.setItem('gym', response.data.id_gym)
-      if (response.status === 200 || response.status === 201) navigate('/home')
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
   const meduzaLogin = require('../../assets/images/meduzaLogin.png')
   const meduzaLoginLogo = require('../../assets/images/meduzaLoginLogo.png')
---
+++ src/views/register/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const Background = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: linear-gradient(
+    252.6deg,
+    rgba(129, 0, 93, 0.71) 2.91%,
+    rgba(87, 32, 72, 0.72) 12.64%,
+    rgba(11, 9, 11, 0.583333) 37.9%,
+    rgba(0, 0, 0, 0.87) 78.09%
+  );
+  background-repeat: no-repeat;
+`
+
+export const ImageLogin = styled.img`
+  width: 500px;
+  height: auto;
+  object-fit: contain;
+`
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 52px;
+  width: 500px;
+  height: 460px;
+  border-radius: 10px;
+  background: linear-gradient(225.95deg, rgba(255, 255, 255, 0.212) 7.47%, rgba(255, 255, 255, 0.053) 96.58%);
+  box-shadow: 0px 4px 24px -1px rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(20px);
+`
+
+export const LoginLogo = styled.img`
+  width: 160px;
+  height: auto;
+  object-fit: contain;
+`
